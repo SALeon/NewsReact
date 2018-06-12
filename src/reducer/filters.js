7225e366f4b1f369e0c24091aaab71b5cb07cc58
Saleon <developleon@gmail.com>
2018-06-12 21:51:56 +0300

    lesson5 add delete article in filter reducer

--- src/reducer/filters.js
+++ src/reducer/filters.js
@@ -1,26 +1,26 @@
-import {FILTER_DATE_RANGE, FILTER_DATE_SELECT} from "../constants";
+import {DELETE_ARTICLE, FILTER_DATE_RANGE, FILTER_DATE_SELECT} from "../constants";
 
 const defaultFilter = {
     dateRange:
         {
             from: null,
-             to: null
+            to: null
         },
     selected: []
 };
 
-export default (filters =  defaultFilter, action) => {
+export default (filters = defaultFilter, action) => {
     const {payload, type} = action;
 
     switch (type) {
         case FILTER_DATE_RANGE:
             return {...filters, dateRange: payload.range};
-    }
 
-    switch (type) {
         case FILTER_DATE_SELECT:
             return {...filters, selected: payload.selected};
-    }
 
+        case DELETE_ARTICLE:
+            return {...filters, selected: filters.selected.filter(id => id !== payload.id)}
+    }
     return filters;
 }
