824451eb20bdab5d5ebcbabf8d0c203c9d84acd7
SALeon <developleon@gmail.com>
2018-07-02 19:02:13 +0300

    add NewArticle

---
+++ src/components/routes/NewArticle.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class NewArticle extends Component {
+    static propTypes = {};
+
+    render () {
+        return (
+            <>
+                <h1>New Article Form</h1>
+            </>
+        )
+    }
+}
+
+export default NewArticle;
